10d18cebd8ec66b56f2fcd3dcac97afa6e452451
a-llison-lau <allison.lau@mail.utoronto.ca>
2024-11-22 00:26:25 -0500

    fixed more typos

--- components/PublicationItem.tsx
+++ components/PublicationItem.tsx
@@ -9,7 +9,7 @@ const PublicationItem = ({publication, index}: Props): JSX.Element => {
     return (
         <div className = "mt-4 mb-8">
             <p className = "text-base text-gray-500">
-                [{index}] {publication.author} <b><i>{publication.title}</i></b><br/>
+                [{index}] <b><i>{publication.title}</i></b> <br/>{publication.author}<br/>
                 <a className = "text-sm">{publication.conference}</a>
             </p>
             <p className = "text-gray-500 flex justify-end text-sm bold">
