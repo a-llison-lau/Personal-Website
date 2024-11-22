3bc93d1146f38a501414050fc7ba0d586c244549
a-llison-lau <allison.lau@mail.utoronto.ca>
2024-11-22 00:42:21 -0500

    added abstract

--- components/PublicationItem.tsx
+++ components/PublicationItem.tsx
@@ -9,8 +9,13 @@ const PublicationItem = ({publication, index}: Props): JSX.Element => {
     return (
         <div className = "mt-4 mb-8">
             <p className = "text-base text-gray-500">
-                [{index}] <b><i>{publication.title}</i></b> <br/>{publication.author}<br/>
+                [{index}] <b><i>{publication.title}</i></b> 
+                <br/>
+                {publication.author}
+                <br/>
                 <a className = "text-sm">{publication.conference}</a>
+                <br/>
+                {publication.abstract}
             </p>
             <p className = "text-gray-500 flex justify-end text-sm bold">
                 {publication.links.map((linkItem: any, idx: any) => (
