632681818e2ee4440264e326c9603ce497e3c3de
a-llison-lau <allison.lau@mail.utoronto.ca>
2024-12-12 14:36:38 -0800

    fixed compile error

--- components/News.tsx
+++ components/News.tsx
@@ -1,12 +1,12 @@
-const Teaching = (): JSX.Element => {
+const News = (): JSX.Element => {
     return (
-        <section className="grid" id="teaching">
+        <section className="grid" id="news">
             <h2 className="text-xl font-bold mt-12 mb-4">Teaching</h2>
             <div className="text-base ml-4 text-gray-700">
                 <ul className="list-disc">
                   {
                     <li>
-                        Dec 12: I am in Vancouver for NeurIPS! I'll be presenting at the Adaptive Foundation Models Workshop on Dec 14 (Sat) at 4:30-5:30pm: Personalized Adaption via In-Context Preference Learning
+                        <strong>Dec 12:</strong>: I am in Vancouver for NeurIPS! I'll be presenting at the Adaptive Foundation Models Workshop on Dec 14 (Sat) at 4:30-5:30pm: Personalized Adaption via In-Context Preference Learning
                         <a href="https://adaptive-foundation-models.org/posters/PPT_AFM_2024_Poster.pdf">
                         Personalized Adaption via In-Context Preference Learning
                       </a>
