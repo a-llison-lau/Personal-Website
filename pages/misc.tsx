768dedc69713afce322ad42277889185a66b699c
a-llison-lau <allison.lau@mail.utoronto.ca>
2024-12-28 00:39:13 -0500

    changed misc layout

--- pages/misc.tsx
+++ pages/misc.tsx
@@ -4,9 +4,18 @@ import photos from '../components/data/photos.json';
 
 const Misc: NextPage<unknown> = () => {
     const [loading, setLoading] = useState(true);
+    const [modalImage, setModalImage] = useState<string | null>(null);
+
+    const openModal = (src: string) => {
+        setModalImage(src);
+    };
+
+    const closeModal = () => {
+        setModalImage(null);
+    };
 
     return (
-        <div className="min-h-screen px-4 py-8">
+        <div className="min-h-screen px-4 py-8 relative">
             <p className="text-base text-left mb-4">Also, I take pictures :D</p>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {photos.map((photo, index) => (
@@ -14,12 +23,32 @@ const Misc: NextPage<unknown> = () => {
                         <img 
                             src={photo.src} 
                             alt={photo.alt} 
-                            className="w-full h-auto object-cover transition-transform duration-300 hover:scale-105" 
+                            className="w-full h-auto object-cover transition-transform duration-300 hover:scale-105 cursor-pointer" 
+                            onClick={() => openModal(photo.src)}
                             onLoad={() => setLoading(false)}
                         />
                     </div>
                 ))}
             </div>
+
+            {/* Modal */}
+            {modalImage && (
+                <div 
+                    className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50"
+                    onClick={closeModal} // Close modal when background is clicked
+                >
+                    <div 
+                        className="relative"
+                        onClick={(e) => e.stopPropagation()} // Prevent background click from closing modal
+                    >
+                        <img 
+                            src={modalImage} 
+                            alt="Full Screen" 
+                            className="max-w-full max-h-full object-contain"
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
