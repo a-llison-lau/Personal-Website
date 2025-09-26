95007e5a605d8f0159a91d0359ca031c6abcc503
a-llison-lau <allison.lau@mail.utoronto.ca>
2025-09-26 10:56:41 +0200

    updated about me

--- components/MoreAbout.tsx
+++ components/MoreAbout.tsx
@@ -6,8 +6,8 @@ const MoreAbout = (): JSX.Element => {
         just to master a subject, but to uncover new ways of seeing. I am an
         explorer at heart, driven by a curiosity to understand the world,
         humans, intelligent systems, and the relationship between the artificial
-        and the natural. My work spans diverse research domains, including
-        causal effect estimation and systems security in LLMs, computational
+        and the natural. My previous work spans diverse research domains,
+        including causal effect estimation and security in LLMs, computational
         sensing, medical computer graphics, and interdisciplinary science and
         instrumentation. Outside of deep tech, I am exploring innovation and
         venture. In my free time, I like to enjoy the outdoors and learn
