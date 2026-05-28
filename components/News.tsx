b35050ecda2678b29b574e5cb235b163d375d98e
a-llison-lau <allison.lau@mail.utoronto.ca>
2026-05-28 22:47:17 +0200

    updated memory over maps news

--- components/News.tsx
+++ components/News.tsx
@@ -1,27 +1,39 @@
-import ExtLink from './ExtLink';
+import ExtLink from "./ExtLink";
 
 const News = (): JSX.Element => {
-    return (
-        <section className="grid mt-8" id="news">
-            <h1 className="text-2xl font-bold text-left mb-4">News</h1>
-            <div className="text-base ml-4 mb-8">
-                <ul className="list-disc">
-                {
-                    <li>
-                        <strong>25' Sep 15</strong>:: I started my master's at ETH Zurich!
-                    </li>
-                  }
-                  {
-                    <li>
-                        <strong>24' Dec 10</strong>:: I am in Vancouver for NeurIPS! I'll be presenting a poster on Dec 14 (Sat) at 4:30-5:30pm: <ExtLink href= "https://adaptive-foundation-models.org/posters/PPT_AFM_2024_Poster.pdf">Personalized Adaptation via In-Context Preference Learning </ExtLink> @ <ExtLink href= "https://adaptive-foundation-models.org/index.html">AFM </ExtLink>
-                    </li>
-                  }
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section className="grid mt-8" id="news">
+      <h1 className="text-2xl font-bold text-left mb-4">News</h1>
+      <div className="text-base ml-4 mb-8">
+        <ul className="list-disc">
+          {
+            <li>
+              <strong>26' May</strong>:: Our paper Memory Over Maps is accepted
+              to the MM-SpatialAI Workshop at ICRA 2026 as an oral!
+            </li>
+          }
+          {
+            <li>
+              <strong>25' Sep</strong>:: I started my master's at ETH Zurich!
+            </li>
+          }
+          {
+            <li>
+              <strong>24' Dec</strong>:: I am in Vancouver for NeurIPS! I'll be
+              presenting a poster on Dec 14 (Sat) at 4:30-5:30pm:{" "}
+              <ExtLink href="https://adaptive-foundation-models.org/posters/PPT_AFM_2024_Poster.pdf">
+                Personalized Adaptation via In-Context Preference Learning{" "}
+              </ExtLink>{" "}
+              @{" "}
+              <ExtLink href="https://adaptive-foundation-models.org/index.html">
+                AFM{" "}
+              </ExtLink>
+            </li>
+          }
+        </ul>
+      </div>
+    </section>
+  );
 };
-
-
 
 export default News;
